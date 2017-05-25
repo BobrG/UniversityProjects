fe7c7102570abbe4875f63e90b05b9744f35803f
Gleb Bobrovskikh <bobrovskihg@gmail.com>
2017-05-25 18:17:37 +0300

    functions described

--- Ecosys.cpp
+++ Ecosys.cpp
@@ -1,17 +1,71 @@
 #include "Ecosys.h"
 
-void Object::handle_modification() {
-	for (size_t i = 0; i < _Features.size(); ++i) {
-		_Features[i]->update();
+void Modifier::connect(Feature* _Feature) {
+	dependers_f.push_back(_Feature);
+}
+void Modifier::connect(Modifier* _Modifier) {
+	dependers_m.push_back(_Modifier);
+}
+
+
+void Modifier::disconnect(Feature* _Feature) {
+	dependers_f.erase(std::remove(dependers_f.begin(), dependers_f.end(), _Feature), dependers_f.end());
+}
+void Modifier::disconnect(Modifier* _Modifier) {
+	dependers_m.erase(std::remove(dependers_m.begin(), dependers_m.end(), _Modifier), dependers_m.end());
+}
+
+void Modifier::update(Modifier& _Modifier, void* what = 0) {
+	// change value
+	_Modifier.notify();
+}
+
+void Modifier::notify() {
+	for (size_t i = 0; i < dependers_f.size(); ++i) {
+		dependers_f[i]->update(*this);
+	}
+	for (size_t i = 0; i < dependers_m.size(); ++i) {
+		dependers_m[i]->update(*this);
 	}
 }
 
-void Container::notify() {
-	for (size_t i = 0; i < _Objects.size(); ++i) {
-		_Objects[i]->handle_modification();
+Feature::Feature() {
+	// define values;
+}
+
+void Feature::update(Modifier& _Modifier, void* what = 0) {
+	// change value;
+}
+
+void Object::Init() {
+	// define Object's values, start features, etc;
+}
+
+void Object::add_feature(Feature* _Feature) {
+	_Features.push_back(_Feature);
+}
+void Object::remove_feature(Feature* _Feature) {
+	_Features.erase(std::remove(_Features.begin(), _Features.end(), _Feature), _Features.end());
+}
+
+void Container::Init() {
+	// define Container's values, start features, start modifiers  etc;
+}
+
+void Container::set_headmaster(Container* _Container) {
+	Headmaster = _Container;
+}
+
+void Container::add_modifier(Modifier* _Modifier) {
+	_Modifiers.push_back(_Modifier);
+}
+void Container::add_container(Container* _Container) {
+	_Container->set_headmaster(this);
+	_Containers.push_back(_Container);
+	for (int i = 0; i < _Container->_Modifiers.size(); ++i) {
+//		_Container->_Modifiers[i]->connect();
 	}
-	for (size_t i = 0; i < _Containers.size(); ++i) {
-		_Containers[i]->handle_modification();
-		_Containers[i]->notify();
-	}
+}
+void Container::add_object(Object* _Object) {
+	_Objects.push_back(_Object);
 }
--- Ecosys.h
+++ Ecosys.h
@@ -1,27 +1,55 @@
 #pragma once
 #include <vector>
+#include <algorithm>
 #include <iostream>
+
+// So, I understand the structure as follows:
+// Our smallest constituent unit is Feature. Features are subscribers or observers. They only react
+// on changes which come from other unit - Modifier. Modifier is a Feature which can influence other Features.
+// The interaction between Features and Modifiers is defined by Containers and Objects. 
+// And here comes the User, who defines the hierarchy of Containers and Objects in particular case. 
+// In console the User can choose what each Container stores - what Objects and what Containers. 
+// Features of various Objects and Containers are define at the start, however some new Features can be 
+// gathered during the work of the programm. 
+// In the nutshell the structure can be described in some kind of a tree:
+// C1 --stores--> O1 && (C2 --stores--> O2 && O3)
+// C1 (ex. room) possess Feature 1 (ex.colour), Feature 2 (ex.comfortable), Modifier 1 (ex.heat 100 degrees), Modifier 2 (ex.entertainment Mr.Robot season 2);
+// O1 (ex. human) possess Feature 1 (ex.geek), Feature 2 (ex.colour);
+// C2 (ex. bottle) possess Feature 1 (ex. colour), Modifier 1 (ex.open/closed);
+// O2 (ex. water) possess Feature 1 (ex.heat 10 degrees), Feature 2 (ex.condition liquid);
+// O3 (ex. ice) possess Feature 1 (ex.heat -10 degrees), Feature 2 (ex.condition solid);
+// What modifier Open of C2 means for us? It means that when the modifier of Heat will change it will influence on temperature of O2 or O3 
+// without any obstacle. And if User will change the modifier Open on Closed then the changes in Heat on higher levels will be 
+// monitored by modifier Closed on the lower levels.
+// IMPORTANT!!! The pattern Pub/Sub is requiered to prevent Modifiers which have no relation with some Features influence them. 
+// (ex.Mr.Robot TV show can influence on Geek feature on Human object, but Feature of Colour stays indiferent to this Modifier)
 
 // Each modficator can attach only feature from the depth but cannot affect features which are higher in the list
 // of containers and object!!!
 // So that we can create connections of modifiers and features from container so that they do not know about
 // features from the head!!!
 
-
 // modifier inherits methods of feature class because it can be described as a feature with possibility of
 // influence. This relations between features and modifiers are important because modifiers can influence on other modifiers.
+class Feature;
 
 class Modifier { // class of modifiers which influence on each object or container
 public:
+
 	void connect(Feature* _Feature);
+	void connect(Modifier* _Modifier);
 
 	void disconnect(Feature* _Feature);
+	void disconnect(Modifier* _Modifier);
 
-	void change_status(void* what);
+	virtual void update(Modifier& _Modifier, void* what = 0);
 
-	virtual void notify(Feature* _Feature = 0, void* what = 0);
+	void notify();
+
+	virtual ~Modifier() = 0;
 private:
-	std::vector<Feature*> dependers;
+	std::vector<Feature*> dependers_f;
+	std::vector<Modifier*> dependers_m;
 };
 
 
@@ -31,33 +59,36 @@ public:
 
 	virtual void update(Modifier& _Modifier, void* what = 0);
 	
-	virtual ~Feature();
+	virtual virtual ~Feature() = 0;
 };
 
-class Object {
+class Object { // object which contain features only
 public:
-	Object();
+	virtual void Init();
 
 	void add_feature(Feature* _Feature); 
 
 	void remove_feature(Feature* _Feature);
 
-	void handle_modification(); // object gets a signal about changes in container and handle event;
 private:
 	std::vector<Feature*> _Features;
 };
 
-class Container: public Object {
+class Container: public Object { // container inherits ability of holding features from object and adds ability of modifying features;
 public:
-	Container(); 
+	void Init() { Headmaster = NULL; }
 
-	void notify();
+	//void notify();
+	void set_headmaster(Container* _Container);
 
-	void add_object(Object& _Object); // add and affect with modificator; ???
+	void add_modifier(Modifier* _Modifier);
 
-	void add_container(Container& _Container);
+	void add_object(Object* _Object); // add and affect with modificator; ???
+
+	void add_container(Container* _Container);
 private:
 	std::vector<Container*> _Containers;
 	std::vector<Object*> _Objects;
 	std::vector<Modifier*> _Modifiers;
+	Container* Headmaster;
 };

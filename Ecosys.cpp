3019ec31f510bd9f721c7c22a7c7750d5b1a7b97
Gleb Bobrovskikh <bobrovskihg@gmail.com>
2017-05-27 14:06:58 +0300

    added some updates, expanded methods

--- Ecosys.cpp
+++ Ecosys.cpp
@@ -1,9 +1,10 @@
 #include "Ecosys.h"
 
-void Modifier::set_type(std::string _type) { type = _type; }
+void Modifier::add_type(std::string _type) { type.push_back(_type); }
+void Modifier::set_name(std::string _name) { name = _name; }
 
 bool Modifier::connect(Feature* _Feature) {
-	if (type.compare(_Feature->get_type())) {
+	if (std::find(type.begin(),type.end(),_Feature->get_type()) != type.end()) {
 		dependers_f.push_back(_Feature);
 		return true;
 	}
@@ -11,7 +12,15 @@ bool Modifier::connect(Feature* _Feature) {
 		return false;
 }
 bool Modifier::connect(Modifier* _Modifier) {
-	dependers_m.push_back(_Modifier);
+	std::vector<std::string> tmp;
+	_Modifier->get_type(tmp);
+	for (size_t i = 0; i < tmp.size(); ++i) {
+		if (std::find(type.begin(), type.end(), tmp) != type.end()) {
+			dependers_m.push_back(_Modifier);
+			return true;
+		}
+	}
+	return false;
 }
 
 
@@ -22,32 +31,46 @@ void Modifier::disconnect(Modifier* _Modifier) {
 	dependers_m.erase(std::remove(dependers_m.begin(), dependers_m.end(), _Modifier), dependers_m.end());
 }
 
-void Modifier::update(Modifier& _Modifier) {
+void Modifier::update(Modifier* _Modifier, void* what = 0) {
 	// change values;
-	_Modifier.notify();
+	std::cout << "Modifier " << _Modifier->get_name() << " changed." <<std::endl;
+	_Modifier->notify();
 }
 
-void Modifier::notify() {
+void Modifier::notify(void* what = 0) {
 	for (size_t i = 0; i < dependers_f.size(); ++i) {
-		dependers_f[i]->update(*this);
+		dependers_f[i]->update(this);
 	}
 	for (size_t i = 0; i < dependers_m.size(); ++i) {
-		dependers_m[i]->update(*this);
+		dependers_m[i]->update(this);
 	}
 }
 
-Feature::Feature() {
+void Feature::Init() {
 	// define values;
+	name = "abstract";
+	type = "abstract";
 }
 
 void Feature::set_type(std::string _type) { type = _type; }
 
-void Feature::update(Modifier& _Modifier, void* what = 0) {
-	// change value;
+void Temperature_Feature::Init() {
+	name = "temperature";
+	type = "temperature";
+	degrees_c = 0;
+}
+
+void Temperature_Feature::update(Modifier* _Modifier, void* what = 0) {
+
 }
 
 void Object::Init() {
 	// define Object's values, start features, etc;
+	_Features.resize(0);
+}
+
+void Object::set_headmaster(Container* _Container) {
+	Headmaster = _Container;
 }
 
 void Object::add_feature(Feature* _Feature) {
@@ -57,8 +80,20 @@ void Object::remove_feature(Feature* _Feature) {
 	_Features.erase(std::remove(_Features.begin(), _Features.end(), _Feature), _Features.end());
 }
 
+Feature* Object::get_feature(int i) {
+	if (i < _Features.size()) {
+		return _Features[i];
+	}
+	else
+		return NULL;
+}
+
 void Container::Init() {
 	// define Container's values, start features, start modifiers  etc;
+	_Modifiers.resize(0);
+	_Features.resize(0);
+	_Objects.resize(0);
+	_Containers.resize(0);
 }
 
 void Container::set_headmaster(Container* _Container) {
@@ -81,5 +116,14 @@ void Container::add_container(Container* _Container) {
 	}
 }
 void Container::add_object(Object* _Object) {
+	_Object->set_headmaster(this);
 	_Objects.push_back(_Object);
+	Feature* tmp_f;
+	for (size_t i = 0; (tmp_f = _Object->get_feature(i)) != NULL; ++i) {
+		for (Container* tmp = this; tmp != NULL; tmp = tmp->Headmaster) {
+			for (size_t j = 0; j < _Modifiers.size(); ++j) {
+				tmp->_Modifiers[j]->connect(tmp_f);
+			}
+		}
+	}
 }
--- Ecosys.h
+++ Ecosys.h
@@ -4,6 +4,9 @@
 #include <algorithm>
 #include <iostream>
 
+// Implement various argument-values system!
+
+
 // So, I understand the structure as follows:
 // Our smallest constituent unit is Feature. Features are subscribers or observers. They only react
 // on changes which come from other unit - Modifier. Modifier is a Feature which can influence other Features.
@@ -14,13 +17,13 @@
 // gathered during the work of the programm. 
 // In the nutshell the structure can be described in some kind of a tree:
 // C1 --stores--> O1 && (C2 --stores--> O2 && O3)
-// C1 (ex. room) possess Feature 1 (ex.colour), Feature 2 (ex.comfortable), Modifier 1 (ex.heat 100 degrees), Modifier 2 (ex.entertainment Mr.Robot season 2);
+// C1 (ex. room) possess Feature 1 (ex.colour), Feature 2 (ex.comfortable), Modifier 1 (ex.Temperature 100 degrees), Modifier 2 (ex.entertainment Mr.Robot season 2);
 // O1 (ex. human) possess Feature 1 (ex.geek), Feature 2 (ex.colour);
 // C2 (ex. bottle) possess Feature 1 (ex. colour), Modifier 1 (ex.open/closed);
-// O2 (ex. water) possess Feature 1 (ex.heat 10 degrees), Feature 2 (ex.condition liquid);
-// O3 (ex. ice) possess Feature 1 (ex.heat -10 degrees), Feature 2 (ex.condition solid);
-// What modifier Open of C2 means for us? It means that when the modifier of Heat will change it will influence on temperature of O2 or O3 
-// without any obstacle. And if User will change the modifier Open on Closed then the changes in Heat on higher levels will be 
+// O2 (ex. water) possess Feature 1 (ex.Temperature 10 degrees), Feature 2 (ex.condition liquid);
+// O3 (ex. ice) possess Feature 1 (ex.Temperature -10 degrees), Feature 2 (ex.condition solid);
+// What modifier Open of C2 means for us? It means that when the modifier of Temperature will change it will influence on temperature of O2 or O3 
+// without any obstacle. And if User will change the modifier Open on Closed then the changes in Temperature on higher levels will be 
 // monitored by modifier Closed on the lower levels.
 // IMPORTANT!!! The pattern Pub/Sub is requiered to prevent Modifiers which have no relation with some Features influence them. 
 // (ex.Mr.Robot TV show can influence on Geek feature on Human object, but Feature of Colour stays indiferent to this Modifier)
@@ -32,12 +35,19 @@
 
 // modifier inherits methods of feature class because it can be described as a feature with possibility of
 // influence. This relations between features and modifiers are important because modifiers can influence on other modifiers.
+
+// ADD DERIVED CLASSES!!!
+
 class Feature;
 
+/*template <class eff_type>*/
 class Modifier { // class of modifiers which influence on each object or container
 public:
-	virtual void set_type(std::string _type);
-	std::string get_type() { return type; }	
+	void set_name(std::string _name);
+	std::string get_name() { return name; }
+
+	virtual void add_type(std::string _type);
+	void get_type(std::vector<std::string> _type) { _type = type; }
 
 	bool connect(Feature* _Feature);
 	bool connect(Modifier* _Modifier);
@@ -45,34 +55,59 @@ public:
 	void disconnect(Feature* _Feature);
 	void disconnect(Modifier* _Modifier);
 
-	virtual void update(Modifier& _Modifier);
+	virtual void update(Modifier* _Modifier, std::string what = 0);
 
-	void notify();
+	virtual void notify(std::string what = 0);
 
 	virtual ~Modifier() = 0;
-private:
-	std::string type;
+protected:
+	std::string name;
+	std::vector<std::string> type;
+	//eff_type eff;
 	std::vector<Feature*> dependers_f;
 	std::vector<Modifier*> dependers_m;
+};
+
+class Temperature_Modifier : public Modifier {
+public:
+	void notify(std::string what = 0);
+
+private:
+	double eff;
 };
 
 
 class Feature { // class of features which define every object or container 
 public:
-	Feature();
+	virtual void Init();
 
 	virtual void set_type(std::string _type);
 	std::string get_type() { return type; }
 
-	virtual void update(Modifier& _Modifier, void* what = 0);
+	virtual void set_value(std::string what) = 0;
+
+	virtual void update(Modifier* _Modifier, std::string what = 0) = 0;
 	
 	virtual virtual ~Feature() = 0;
-private:
+protected:
+	std::string name;
 	std::string type;
-	// consider next:
-	// Modifier* _Modifier;
-	// or maybe we do not need it as we use Modifier as argument in update function;
+
+	std::string handle_name; 
 };
+
+class Temperature_Feature : public Feature {
+public:
+	void Init();
+
+	void set_value(std::string what) { degrees_c = std::stod(what); }
+	
+	void update(Modifier* _Modifier, std::string what = 0);
+private:
+	double degrees_c;
+};
+
+class Container;
 
 class Object { // object which contain features only
 public:
@@ -82,17 +117,22 @@ public:
 
 	void remove_feature(Feature* _Feature);
 
-protected:
+	void set_headmaster(Container* _Container);
+
+	Feature* get_feature(int i);
+
+private:
+	Container* Headmaster;
 	std::vector<Feature*> _Features;
 };
 
-class Container: public Object { // container inherits ability of holding features from object and adds ability of modifying features;
+class Container { // container inherits ability of holding features from object and adds ability of modifying features;
 public:
 	void Init() { Headmaster = NULL; }
 
 	void set_name(std::string& tmp) { name = tmp; }
 	std::string get_name() { return name; }
-	
+
 	void set_headmaster(Container* _Container);
 
 	void add_modifier(Modifier* _Modifier);
@@ -102,8 +142,10 @@ public:
 	void add_container(Container* _Container);
 private:
 	std::string name;
+	std::vector<Feature*> _Features;
 	std::vector<Container*> _Containers;
 	std::vector<Object*> _Objects;
 	std::vector<Modifier*> _Modifiers;
 	Container* Headmaster;
 };
+
